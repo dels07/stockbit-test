d7cb8db995499bc066e48aed15ce9bcbc28819d3
Kang Dels <deli.soetiawan@gmail.com>
2021-10-07 00:37:29 +0700

    refactor(TEST-3): remove second if

--- test 3/index.js
+++ test 3/index.js
@@ -2,19 +2,18 @@ function findFirstStringInBracket(str) {
   if (!str.length) return '';
 
   let indexFirstBracketFound = str.indexOf("(");
-  if (indexFirstBracketFound >= 0) {
-    let wordsAfterFirstBracket = str.substr(indexFirstBracketFound);
-    if (wordsAfterFirstBracket) {
-      wordsAfterFirstBracket = wordsAfterFirstBracket.substr(1);
-      let indexClosingBracketFound = wordsAfterFirstBracket.indexOf(")");
-      if (indexClosingBracketFound >= 0) {
-        return wordsAfterFirstBracket.substring(0,
-          indexClosingBracketFound);
-      }
-      else {
-        return '';
-      }
-    } else {
+
+  if (indexFirstBracketFound < 0) return '';
+
+  let wordsAfterFirstBracket = str.substr(indexFirstBracketFound);
+  if (wordsAfterFirstBracket) {
+    wordsAfterFirstBracket = wordsAfterFirstBracket.substr(1);
+    let indexClosingBracketFound = wordsAfterFirstBracket.indexOf(")");
+    if (indexClosingBracketFound >= 0) {
+      return wordsAfterFirstBracket.substring(0,
+        indexClosingBracketFound);
+    }
+    else {
       return '';
     }
   } else {
