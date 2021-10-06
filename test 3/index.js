a34a1408835ad1d27d3af4a54ac7eeba1a264379
Kang Dels <deli.soetiawan@gmail.com>
2021-10-07 00:32:22 +0700

    test(TEST-3): add simple assertions

--- test 3/index.js
+++ test 3/index.js
@@ -23,3 +23,16 @@ function findFirstStringInBracket(str) {
     return '';
   }
 }
+
+// add simple assertions
+const result1 = findFirstStringInBracket("");
+const result2 = findFirstStringInBracket("foobar");
+const result3 = findFirstStringInBracket("(foo)bar");
+const result4 = findFirstStringInBracket("foo(bar)");
+const result5 = findFirstStringInBracket("(foo)(bar)");
+
+console.log(result1 === '' ? 'passed' : 'failed');
+console.log(result2 === '' ? 'passed' : 'failed');
+console.log(result3 === 'foo' ? 'passed' : 'failed');
+console.log(result4 === 'bar' ? 'passed' : 'failed');
+console.log(result5 === 'foo' ? 'passed' : 'failed');
